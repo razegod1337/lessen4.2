ff927a63a634307c5d6ddaad07faba11bf7047d9
razegod1337 <serzh.zhdanov19@gmail.com>
2023-08-04 15:21:39 +0300

    new

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -3,8 +3,8 @@ public class CreditPaymentService {
         double percentMonth;
         percentMonth = interestRate / 1200;
         double month;
-        month = years *12;
-        double monthPay = sum * percentMonth/(1-Math.pow(1+percentMonth,- month));
+        month = years * 12;
+        double monthPay = sum * percentMonth / (1 - Math.pow(1 + percentMonth, -month));
         return (int) monthPay;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double sum =  100_00_00;
+        double sum = 100_00_00;
         double interestRate = 9.99;
-        double years = 1;
-        double monthPay = service.calculate(sum, interestRate, years);
+        double[] years;
+        years = new double[3];
+        years[0] = 1;
+        years[1] = 2;
+        years[2] = 3;
+        double monthPay = service.calculate(sum, interestRate, years[0]);
+        double monthPayTwo = service.calculate(sum, interestRate, years[1]);
+        double monthPayThree = service.calculate(sum, interestRate, years[2]);
         System.out.println(monthPay);
+        System.out.println(monthPayTwo);
+        System.out.println(monthPayThree);
     }
 }
